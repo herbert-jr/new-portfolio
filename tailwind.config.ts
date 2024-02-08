b2b16d5bcde598f251ac032c017539eb67f9a64c
Jjjunior7 <herbert18h@gmail.com>
2024-02-08 15:42:02 -0300

    :tada: Commit inicial

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'background-main': '#fff7ed',
+        'background-burger': '#e5ded2',
+        'burger-content': '#292928',
+        copy: '#292928',
+        'copy-light': '#faf5f5',
+        'sub-title': '#464644',
+        primary: '#fc7643',
+        'primary-content': '#3f1201',
+        'primary-dark': '#fb5111',
+        'primary-light': '#fd9b75',
+        secondary: '#273248',
+        'secondary-content': '#a2b0cc',
+        'secondary-dark': '#151b27',
+        'secondary-light': '#394969',
+        logo: '#ffcea9',
+        menu: '#AF4F41',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
